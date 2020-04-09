a925b22549d1bab0da5f5e64ffd2f698c3f5cb49
zrong <1174064549@qq.com>
2020-04-09 21:08:06 +0800

    初始化项目

---
+++ android/app/src/main/kotlin/com/th580/jianshu/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.th580.jianshu
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
